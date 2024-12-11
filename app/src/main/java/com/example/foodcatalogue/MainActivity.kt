462c976eb26604ff0b758db3f84234f098274372
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2024-12-11 17:33:57 +0700

    feat: integrate recyclerview adapter and dummy data

--- app/src/main/java/com/example/foodcatalogue/MainActivity.kt
+++ app/src/main/java/com/example/foodcatalogue/MainActivity.kt
@@ -1,25 +1,51 @@
 package com.example.foodcatalogue
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodcatalogue.data.Food
+import com.example.foodcatalogue.data.foodList
 import com.example.foodcatalogue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val listFood = ArrayList<Food>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.rvFoods.setHasFixedSize(true)
+        listFood.addAll(foodList)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        binding.rvFoods.layoutManager = LinearLayoutManager(this)
+        val listFoodAdapter = ListFoodAdapter(listFood)
+        binding.rvFoods.adapter = listFoodAdapter
+
+        listFoodAdapter.setOnItemClickCallback(object : ListFoodAdapter.OnItemClickCallback {
+            override fun onItemClicked(food: Food) {
+                showSelectedFood(food)
+            }
+
+        })
+    }
+
+    private fun showSelectedFood(food: Food) {
+        Toast.makeText(this, "Kamu memilih " + food.name, Toast.LENGTH_SHORT).show()
     }
 }
