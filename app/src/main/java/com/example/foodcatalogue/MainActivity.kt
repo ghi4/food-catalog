f7f1d4e8e8546a8f6759227148da479dacd52ef0
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2024-12-13 21:25:03 +0700

    feat: implement menu for action profile

--- app/src/main/java/com/example/foodcatalogue/MainActivity.kt
+++ app/src/main/java/com/example/foodcatalogue/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.foodcatalogue
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -34,6 +36,21 @@ class MainActivity : AppCompatActivity() {
         binding.rvFoods.setHasFixedSize(true)
         listFood.addAll(foodList)
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_profile -> {
+                val intent = Intent(this@MainActivity, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showRecyclerList() {
